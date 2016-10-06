319302f4f48b43418a45788a31f110f33bd0e324
sk151 <serg3y@gmail.com>
2016-10-05 22:30:43 -0600

    Create Calc.java

---
+++ MVC/Calc.java
@@ -0,0 +1,10 @@
+package MVC;
+
+public class Calc {
+    public static void main(String[] args) {
+        CalcView theView = new CalcView();
+        CalcModel theModel = new CalcModel();
+        //Create the controller and pass the view and the model for it to connect
+        CalcController theController = new CalcController(theView,theModel);
+    }
+}
