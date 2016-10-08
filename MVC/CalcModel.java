4fc17a7f6a77aad810a1fcc13cafe471f8d46291
sk151 <serg3y@gmail.com>
2016-10-08 12:24:28 -0600

    Moved all calculations here from the View

--- MVC/CalcModel.java
+++ MVC/CalcModel.java
@@ -2,16 +2,22 @@ package MVC;
 
 // This Model does all the calculations
 public class CalcModel {
+    // Holds the value of the sum of the numbers
+    // entered in the view
     private double result;
     
-    public void doMath(double firstNumber, double secondNumber, String operation) {
+    // This method decides which operation to do depending on user input
+    // and calls the appropriate function
+    public void doMath(String firstNumber, String secondNumber, String operation) {
     	switch (operation){
-        case "+": this.add(firstNumber, secondNumber); break;
-        case "-": this.substract(firstNumber, secondNumber); break;
-        case "*": this.multiply(firstNumber, secondNumber); break;
-        case "/": this.divide(firstNumber, secondNumber); break;
+        case "+": this.add(Double.parseDouble(firstNumber), Double.parseDouble(secondNumber)); break;
+        case "-": this.substract(Double.parseDouble(firstNumber), Double.parseDouble(secondNumber)); break;
+        case "*": this.multiply(Double.parseDouble(firstNumber), Double.parseDouble(secondNumber)); break;
+        case "/": this.divide(Double.parseDouble(firstNumber), Double.parseDouble(secondNumber)); break;
         }
     }
+    
+    // Calculations
     
     public void add(double firstNumber, double secondNumber){
     	result = firstNumber + secondNumber;
