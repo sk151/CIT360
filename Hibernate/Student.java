b64b08a95be09d15fc3642f00fdf9f20eca97903
sk151 <serg3y@gmail.com>
2016-10-25 21:19:57 -0600

    Create Student.java

---
+++ Hibernate/Student.java
@@ -0,0 +1,49 @@
+package hibernate;
+
+// A simple class to work with
+public class Student {
+	private int id;
+	private String firstName; 
+	private String lastName;   
+	private float gpa;  
+
+	public Student() {}
+	public Student(String fn, String ln, int gpa) {
+		this.firstName = fn;
+		this.lastName = ln;
+		this.gpa = gpa;
+	}
+	
+	// Getters ad setters for the class
+	public int getId() {
+		return id;
+	}
+	
+	public void setId( int id ) {
+      this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName( String fn) {
+		this.firstName = fn;
+   }
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName( String ln ) {
+		this.lastName = ln;
+	}
+	
+	public float getGPA() {
+		return gpa;
+	}
+	
+	public void setGPA(float g) {
+		this.gpa = g;
+	}
+}
