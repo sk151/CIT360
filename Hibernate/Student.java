f8aadc0fc647af7d67f3e51ce9cb9f6adb90d613
sk151 <serg3y@gmail.com>
2016-10-25 21:27:11 -0600

    Update Student.java

--- Hibernate/Student.java
+++ Hibernate/Student.java
@@ -1,4 +1,10 @@
-package hibernate;
+package org.myHibernate.java.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 // A simple class to work with
 public class Student {
