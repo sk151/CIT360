95936adea488ff159ae4323074e5fbb811cc0a3c
sk151 <serg3y@gmail.com>
2016-11-24 15:01:30 -0700

    Update Test.java

--- JUnit/Test.java
+++ JUnit/Test.java
@@ -8,13 +8,29 @@ import static org.junit.Assert.assertEquals;
 
 public class Test {
 	
-   int number1 = 1;	
-   int number2 = 2;
-   AddNumbers addNumbers = new AddNumbers(number1, number2);
-
-   @Test
-   public void testAddNumbers() {
-      assertEquals(3,addNumbers.Add(number1, number2));
+	@Before // Setting everything up for testing
+	public void inputData() {
+		int number1 = 1;	
+   		int number2 = 2;
+  		AddNumbers addNumbers = new AddNumbers(number1, number2);
+		System.out.println("Test object initialized.");
+	}
+	
+	@Ignore // An old version of the test I'm ignoring this time
+	public void testAddNumbers1() {
+      		assertEquals(number1+number2,addNumbers.Add(number1, number2));
+	
+   	@Test // The current test
+   	public void testAddNumbers2() {
+      		assertEquals(3,addNumbers.Add(number1, number2));
+		System.out.println("Testing.");
+	}
+	
+	@After // Unnecessary here but this is where I'd free up expensive resources etc.
+	public void cleanup() {
+		AddNumbers = null;
+		System.out.println("Test object initialized.");
+	}
       
    }
 }
